348067a0e9dbb1e1bf191ef9734a8715dfead20d
yarencekemci <yarencekemci@gmail.com>
2026-02-26 10:57:26 +0300

    CSharpPractice-26.02.2026

---
+++ 260226_1_CSharpPractice_5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _260226_1_CSharpPractice_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           while(true)
+            {
+                Console.WriteLine("Kelime girin / (Programdan cikmak istediginizde 1'e basiniz):");
+                string kelime = Console.ReadLine();
+
+                if (kelime == "1")
+                    break;
+
+                string tersKelime = KelimeTersCevir(kelime);
+                Console.WriteLine("Ters cevrilmis kelime: " + tersKelime);
+            }
+            Console.WriteLine("Programdan cikiliyor");
+            Console.ReadLine();
+        }
+        static string KelimeTersCevir(string kelime)
+        {
+            string ters = "";
+
+            for (int i = kelime.Length - 1; i >= 0; i--)
+            {
+                ters += kelime[i]; 
+            }
+
+            return ters;
+        }
+    }
+}
