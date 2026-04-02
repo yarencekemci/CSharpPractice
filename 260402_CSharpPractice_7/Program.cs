536f92d5a8d0c16ebd1c2055c38e8f6b3e7f79fa
yarencekemci <yarencekemci@gmail.com>
2026-04-02 12:40:39 +0300

    2.04.2026

---
+++ 260402_CSharpPractice_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _260402_CSharpPractice_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan kullanıcı adı ve şifre alın. Eğer kullanıcı adı '' admin '' şifre ''123'' ise
+            // Ekrana true/false yazdırın.
+
+            string kullaniciAdi;
+            string sifre;
+
+            Console.WriteLine("kullanici adi giriniz: ");
+            kullaniciAdi = Console.ReadLine();
+
+            Console.WriteLine("sifre giriniz: ");
+            sifre = Console.ReadLine();
+
+            bool kontrol = kullaniciAdi == "admin" && sifre == "123";
+            
+            Console.WriteLine("bilgileriniz dogru mu? " + kontrol);
+
+
+        }
+    }
+}
